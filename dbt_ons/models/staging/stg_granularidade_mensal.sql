d4c69bb1c05ead9fe58d5afbcf7b834e8270058f
afranio-viana <afranio.esquerdo.viana@gmail.com>
2026-01-26 22:04:31 -0400

    chore: ajusta stg_granularidade_mensal para mostrar ano e mes

--- dbt_ons/models/staging/stg_granularidade_mensal.sql
+++ dbt_ons/models/staging/stg_granularidade_mensal.sql
@@ -9,6 +9,8 @@ clean AS (
         REGEXP_REPLACE(TRIM(UPPER(id_subsistema)),'\s+',' ','g') AS sigla_regiao,
         REGEXP_REPLACE(TRIM(UPPER(nom_subsistema)),'\s+',' ','g') AS nome_subsistema,
         TO_CHAR(din_instante,'YYYY-MM-DD') AS data_referencia,
+        TO_CHAR(din_instante,'MM') AS mes,
+        TO_CHAR(din_instante,'YYYY') AS ano,
         val_cargaenergiamwmed AS carga_referencia
     FROM raw_table
 )
